ec2ccb9254bdd2a0afb72412f53f779ab32f2a21
Bhavy <bhavypatel.dev@gmail.com>
2024-07-13 20:51:39 +0530

    fir

--- Frontend/src/components/GeneralScreens/Footer.js
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer">
             </div>
             <div className="copyright">
-                <p className="copyright-blog">© 2024 Bhavy Patel. All Rights Reserved</p>
+                <p className="coyright-blog">© 2024 Bhavy Patel. All Rights Reserved</p>
             </div>
         </div>
     )
